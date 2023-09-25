34cb47200fd0ecc3de422e292c15341b84b6dd36
Federico Segú <federicosegu@gmail.com>
2023-09-25 20:29:26 -0300

    primer commit

---
+++ src/productManager.js
@@ -0,0 +1,113 @@
+import fs from 'fs'
+
+
+let products = [];
+class ProductManager {
+    constructor(){
+       this.path = 'Products.json'  
+    } 
+    async getProducts(){
+      try {
+        if(fs.existsSync(this.path)){ 
+          const productsFile = await fs.promises.readFile(this.path, 'utf-8')
+          return JSON.parse(productsFile)
+      } else {
+          return []
+    }
+    } catch (error) {
+        return error
+    }}
+    
+    async addProduct(productoCreado){
+      
+        try { //primero hay que leer el archivo 
+            let id
+            if(!products.length){
+                id = 1
+            } else {
+                id = products[products.length-1].id+1
+            }
+           
+            products.push({id,...productoCreado}) //id + todo lo que venga en user
+            //let products0 = await this.getProducts()
+            await fs.promises.writeFile(this.path, JSON.stringify(products))
+        }
+        catch(error){
+            return error
+        }
+        }
+    async getProductById(id){ 
+      try {
+        const products = await this.getProducts();
+        const product = products.find(u=>u.id === id)
+      if(!product){
+        return 'no product'
+    } else {
+        return product
+    }
+    } catch (error){
+        return error
+    }}
+    
+    async deleteProduct(id){
+        const products = await this.getProducts()
+        const newProducts = products.filter(u=>u.id !== id)
+        await fs.promises.writeFile(this.path, JSON.stringify(newProducts))
+    }
+
+    async updateProduct({id, ...producto}){
+      const products = await this.getProducts()
+      if(products.find(e=> e.id === id)) {
+        await this.deleteProduct(id);
+        const newProducts1= await this.getProducts();
+        const newProducts2 = [{id, ...producto}, ...newProducts1];
+        await fs.promises.writeFile(this.path, JSON.stringify(newProducts2))  
+    } else {
+      console.log(`el ID ${id} que deseas actualizar no existe`)
+      return
+    }}}
+
+
+const producto1 = {
+    title: 'Empanadas',
+    description: 'Comida',
+    price: 500,
+    thumbnail: 'http://...',
+    code: 'Codigo001',
+    stock: 250
+}
+const producto2 = {
+    title: 'Gaseosa',
+    description: 'Bebida',
+    price: 600,
+    thumbnail: 'http://...',
+    code: 'Codigo002',
+    stock: 500
+}
+const producto3 = {
+    title: 'Pizza',
+    description: 'Comida',
+    price: 215,
+    thumbnail: 'http://...',
+    code: 'Codigo003',
+    stock: 400
+}
+const producto = new ProductManager();
+producto.addProduct(producto1);
+producto.addProduct(producto2);
+producto.addProduct(producto3);
+
+console.log(products);
+
+
+// console.log(producto.updateProduct({
+//     title:'fideos',
+//     description: 'comida',
+//     price: 400,
+//     thumbnail: 'http://...',
+//     code: 'Codigo002',
+//     stock: 200,
+//     id: 1
+// }))
+
+export const productManager = new ProductManager()
